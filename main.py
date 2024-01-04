288d3c562215204289492227c3325e8ac44c252a
okdef <owaingrandfield@gmail.com>
2024-01-04 03:42:33 -0600

    Added waitforclose close and main methods.

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 from tkinter import Tk, BOTH, Canvas
 
 class Window:
-    root_widget = Tk()
-    root_widget.title = "NoTitle"
-    canvas = Canvas(root_widget, {"bg" : "white"})
-    canvas.pack()
-    is_running = False
+    def __init__(self,width,height):
+        self.root_widget = Tk()
+        self.root_widget.title("NoTitle")
+        self.canvas = Canvas(self.root_widget, bg = "white")
+        self.canvas.pack(fill=BOTH)
+        self.is_running = False
+        self.root_widget.protocol("WM_DELETE_WINDOW", self.close)
 
-    def redraw():
-        root_widget.update()
-        root_widget.update_idletasks()
-        
+    def redraw(self):
+        self.root_widget.update()
+        self.root_widget.update_idletasks()
+
+    def wait_for_close(self):
+        is_running = True
+        while is_running:
+            self.redraw()
+
+    def close(self):
+        is_running = False
+
+def main(self):
+    win = Window(800, 600)
+    win.wait_for_close()
