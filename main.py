6b3dd146654b5d4dd45f5bec32d8255ae1eb3607
okdef <owaingrandfield@gmail.com>
2024-01-05 04:07:29 -0600

    It's running. ToDo: draw graph

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from tkinter import Tk, BOTH, Canvas
 
+############# Window Class ##########
 class Window:
-    def __init__(self,width,height):
+    def __init__(self,width,height, Title = "Notitle"):
         self.root_widget = Tk()
         self.root_widget.title("NoTitle")
         self.canvas = Canvas(self.root_widget, bg = "white")
@@ -14,13 +15,110 @@ class Window:
         self.root_widget.update_idletasks()
 
     def wait_for_close(self):
-        is_running = True
-        while is_running:
+        self.is_running = True
+        while self.is_running:
             self.redraw()
 
     def close(self):
-        is_running = False
+        self.is_running = False
 
-def main(self):
+    def draw_line(self, line, fill_color):
+        line.draw(self.canvas, fill_color)
+
+##this stores the coordinates of a point##
+class Point:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+
+########## Line Class #############
+class Line:
+    def __init__(self, point1, point2):
+        self.x1 = point1.x
+        self.y1 = point1.y
+        self.x2 = point2.x
+        self.y2 = point2.y
+
+    def draw(self, canvas, fill_color):
+        canvas.create_line(self.x1, self.y1, self.x2, self.y2, fill=fill_color, width=2)
+        canvas.pack(fill=BOTH)
+
+# Cell Class
+
+class Cell:
+    def __init__(self,_win, point1, point2, left_wall = True ,right_wall = True,top_wall = True,bottom_wall = True):
+        self._win = _win
+        self.left_wall = left_wall
+        self.right_wall = right_wall
+        self.top_wall = top_wall
+        self.bottom_wall = bottom_wall
+        self.x1 = point1.x
+        self.x2 = point2.x
+        self.y1 = point1.y
+        self.y2 = point2.y
+
+
+        ##Points##
+        self.tl = Point(self.x1,self.y1)
+        self.tr = Point(self.x2,self.y1)
+        self.bl = Point(self.x1,self.y2)
+        self.br = Point(self.x2,self.y2)
+        self.mid = Point(((self.x1+self.x2)/2), ((self.y1+self.y2)/2))
+        
+        ##Lines##
+        self.left = Line(self.tl, self.bl)
+        self.right = Line(self.tr, self.br)
+        self.top = Line(self.tl, self.tr)
+        self.bottom = Line(self.bl, self.br)
+    
+    def draw(self):
+        if self.left_wall:
+            self.left.draw(self._win.canvas, "black")
+        if self.right_wall:
+            self.right.draw(self._win.canvas, "black")
+        if self.top_wall:
+            self.top.draw(self._win.canvas, "black")
+        if self.bottom_wall:
+            self.bottom.draw(self._win.canvas, "black")
+
+    def draw_move(self,to_cell, undo=False):
+        move_line = Line(self.mid, to_cell.mid)
+        if undo == False:
+            move_line.draw(self._win.canvas, fill_color = "red")
+        else:
+            move_line.draw(self._win.canvas, fill_color = "grey")
+
+class Maze:
+    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win):
+        self.x1 = x1
+        self.y1 = y1
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self.cell_size_x = cell_size_x
+        self.cell_size_y = cell_size_y
+        self.win = win
+        self._create_cells()
+
+    def _create_cells(self):
+        self._cells = [[None] * self.num_rows] * self.num_cols
+        for i in self._cells:
+            for j in i:
+                node_p1 = Point(self.x1,self.y1)
+                node_p2 = Point(self.x1+self.cell_size_x, self.y1+self.cell_size_y)
+                cell_node = Cell(self.win, node_p1, node_p2)
+                j = cell_node
+        
+
+        
+
+
+        
+def main():
     win = Window(800, 600)
+    mz = Maze(0,0,3,3,100,100,win)
+    mz._create_cells()
     win.wait_for_close()
+
+    
+
+main()
